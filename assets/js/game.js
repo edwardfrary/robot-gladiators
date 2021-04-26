91bd2b6f37b0f97459e628a9a36570e0a1fd5e2f
edwardfrary <edwardfrary@gmail.com>
2021-04-26 14:31:29 -0400

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Our robots name is " + playerName);
+
+function fight(){
+    window.alert("The Fight has begun! " + playerName + ", Prepare yourself!");
+}
+
+fight();
